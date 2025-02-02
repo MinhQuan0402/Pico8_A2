5d8ea3fabed823cb5a0f0b397b951b24beafb7bb
MinhQuan0402 <ngminhquan.2004@gmail.com>
2025-02-02 23:40:41 +0800

    update player movement

--- pico-8/Project/Source/player.lua
+++ pico-8/Project/Source/player.lua
@@ -1,12 +1,26 @@
-Player = {x=0,y=0}
+Player = {
+  x = 0,
+  y = 0,
+  fdirX = 1,
+  fdirY = 0,
+  width = 0,
+  height = 0,
+  offsetX = 0,
+  offsetY = 0
+}
+Player._index = Player
 
-function Player:new (x,y,o)
-		self.x = x
-		self.y = y
-  o = o or {}
-  setmetatable(o, self)
-  self.__index = self
-  return o
+function Player:new (x , y, width, height, offsetX, offsetY)
+  local instance = setmetatable({}, self)
+  instance.x = x
+  instance.y = y
+  instance.width = width
+  instance.height = height
+  instance.offsetX = offsetX
+  instance.offsetY = offsetY
+  instance.fdirX = 1
+  instance.fdirY = 0
+  return instance
 end
 
 function Player:movement ()
@@ -23,4 +37,8 @@ function Player:movement ()
   if btn(⬇️) then
   	self.y = self.y + 1
   end
+end
+
+function Player:_draw()
+  spr(1, self.x, self.y)
 end
