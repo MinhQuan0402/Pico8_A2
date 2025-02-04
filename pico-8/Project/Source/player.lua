e6332f9d48443d1127e83413d5100eb467f51a79
MinhQuan0402 <ngminhquan.2004@gmail.com>
2025-02-04 21:01:40 +0800

    update player script

--- pico-8/Project/Source/player.lua
+++ pico-8/Project/Source/player.lua
@@ -1,14 +1,15 @@
 Player = {
   x = 0,
   y = 0,
-  fdirX = 1,
+  fdirX = 0,
   fdirY = 0,
   width = 0,
   height = 0,
   offsetX = 0,
-  offsetY = 0
+  offsetY = 0,
+  currentSprite = 1
 }
-Player._index = Player
+Player.__index = Player
 
 function Player:new (x , y, width, height, offsetX, offsetY)
   local instance = setmetatable({}, self)
@@ -18,31 +19,39 @@ function Player:new (x , y, width, height, offsetX, offsetY)
   instance.height = height
   instance.offsetX = offsetX
   instance.offsetY = offsetY
-  instance.fdirX = 1
+  instance.fdirX = 0
   instance.fdirY = 0
+  instance.currentSprite = 1
   return instance
 end
 
 function Player:movement ()
+  self.fdirX = 0
+  self.fdirY = 0
   if btn(➡️) then
-    self.fdirX = 1;
-  	self.x = self.x + 1
+    self.fdirX = 1
   end
   if btn(⬅️) then
-    self.fdirX = -1;
-  	self.x = self.x - 1
+    self.fdirX = -1
   end
   
   if btn(⬆️) then
-    self.fdirY = -1;
-  	self.y = self.y - 1
+    self.fdirY = -1
   end
   if btn(⬇️) then
-    self.fdirY = 1;
-  	self.y = self.y + 1
+    self.fdirY = 1
   end
+
+  self.x += self.fdirX * 1
+  self.y += self.fdirY * 1
 end
 
 function Player:render()
-  spr(1, self.x, self.y)
+  if self.fdirX == 1 then
+    self.currentSprite = 1
+  end
+  if self.fdirX == -1 then
+    self.currentSprite = 2
+  end
+  spr(self.currentSprite, self.x, self.y)
 end
